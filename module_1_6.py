981c5b1e2721081d11d8befaf5176b3f5d7c18a0
pashkuz903 <pkstr.office@gmail.com>
2024-12-16 14:53:14 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,20 @@
+my_dict = {'Pavel': 1995, 'Elena': 1974, 'Liza': 2024, 'Ivan': 1999}
+print(my_dict)
+print(my_dict['Pavel'])
+my_dict['Grisha'] = 1971
+print(my_dict['Grisha'])
+my_dict.update({'Nastya': 1993,
+                'Kolya': 2006})
+print(my_dict)
+a = my_dict.pop('Ivan')
+print(a)
+print(my_dict)
+my_set = {1, 2.5, 1, 2.5, 'Hello', 'Hello'}
+print(my_set)
+print(my_set.add(5))
+print(my_set.add('Day'))
+print(my_set)
+print(my_set.remove(1))
+print(my_set)
+
+
